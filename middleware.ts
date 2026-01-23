a7422d312c42250dd69351cbfe7dd691d83fc9c0
Yourpost-official <ondaypost@gmail.com>
2026-01-23 14:25:45 +0900

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,3 +0,0 @@
-// This file is disabled in favor of proxy.ts to resolve "Both middleware file and proxy file are detected" build error.
-export default function middleware() {}
-export const config = { matcher: [] };
